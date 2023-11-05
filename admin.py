46a7dea765a04917f404357e79fb171d3953d300
smatthe4 <98066285+smatthe4@users.noreply.github.com>
2023-11-05 16:43:11 -0700

    got admin working

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Game
+
+ 
+ 
+
+# Register your models here so they can be edited in admin panel 
+
+admin.site.register(Game)
