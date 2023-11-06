b99406e4800977f30000268d5caab1fd2d16ddf3
smatthe4 <98066285+smatthe4@users.noreply.github.com>
2023-11-05 18:41:23 -0700

    finally got the picture in detail view working

--- models.py
+++ models.py
@@ -5,5 +5,6 @@ from django.urls import reverse
 
 class Game(models.Model):
         title = models.CharField(max_length=200, blank=False) 
-        review = models.CharField("Review", max_length=200, blank=False)
+        picture = models.CharField("picture link", max_length=1000, blank=False)
         ranking = models.CharField(max_length=200)
+
